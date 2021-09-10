dae42fe32bdbf8a12f47fce2bb4f24419633db87
fouadsan <f.benayad95@gmail.com>
2021-09-10 21:36:40 +0100

    Admin Registering/Unregistering Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import Post
+import django.apps
+
+# from .models import Category, Post
+
+models = django.apps.apps.get_models()
+# print(models)
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+admin.site.unregister(django.contrib.sessions.models.Session)
+
+# admin.site.register(Category)
+# admin.site.register(Post)
 
 
-class BlogAdminArea(admin.AdminSite):
-    site_header = 'Blog Admin Area'
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     fields = ['title', 'author']
 
 
-blog_site = BlogAdminArea(name='BlogAdmin')
-
-blog_site.register(Post)
+# admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(max_length=10, choices=options, default='draft')
+
+    class Meta:
+        verbose_name = ("Post")
+        verbose_name_plural = ("Posts")
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+
