c46b824dd4db832b6073d84cea85433dc51ccc5e
fouadsan <f.benayad95@gmail.com>
2021-09-24 17:45:54 +0100

    Upload CSV File to Admin

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.admin import blog_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogadmin/', blog_site.urls)
 ]
